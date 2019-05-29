26c1a2d6d5d6be361049dd91ac1c2ff824b1dc78
dongbaowen <dongbaowen92@126.com>
2019-05-30 00:23:23 +0800

    .

--- src/components/homePage/HomePage2.vue
+++ src/components/homePage/HomePage2.vue
@@ -35,15 +35,29 @@
     </el-menu>
 
     <!-- 主内容 -->
-    <el-row class="max-height" :gutter="20">
-      <el-col class="max-height content" :offset="3" :span="18">
+    <el-row class="outer-row" :gutter="20">
+      <el-col class="content" :offset="3" :span="18">
+        <!-- 轮播图 -->
         <el-row>
           <el-col class="show-box" :span="24">
             <el-carousel :interval="2000" arrow="always">
-              <el-carousel-item v-for="item in dataimg" :key="item">
-                <img src="../../assets/images/1.jpg">
+              <el-carousel-item v-for="item in dataimg" :key="item.index">
+                <img :src="item.src">
               </el-carousel-item>
             </el-carousel>
+          </el-col>
+        </el-row>
+
+        <!-- 左右分栏 -->
+        <el-row class="main-content" :gutter="0">
+          <!-- 左 -->
+          <el-col :span="16">
+            <div class="content-col left-content"></div>
+          </el-col>
+
+          <!-- 左 -->
+          <el-col :span="8">
+            <div class="content-col right-content"></div>
           </el-col>
         </el-row>
       </el-col>
@@ -59,13 +73,16 @@ export default {
       activeIndex2: "1",
       dataimg: [
         {
-          src: "../../assets/images/1.jpg"
+          index: 1,
+          src: require("../../assets/images/1.jpg")
         },
         {
-          src: "../../assets/images/2.jpg"
+          index: 2,
+          src: require("../../assets/images/2.jpg")
         },
         {
-          src: "../../assets/images/3.jpg"
+          index: 3,
+          src: require("../../assets/images/3.jpg")
         }
       ]
     };
@@ -79,12 +96,31 @@ export default {
 </script>
 
 <style scoped>
+.main-content {
+  margin-top: 30px;
+  min-height: 600px;
+}
+.el-row {
+  margin-left: 0px !important;
+  margin-right: 0px !important;
+}
+.left-content {
+  background-color: red;
+  padding-left: 0px;
+}
+.right-content {
+  background-color: yellow;
+  padding-right: 0px;
+}
+.content-col {
+  height: 100px;
+}
+.outer-row {
+  margin: 61px 0px 0px 0px;
+}
 .show-box {
   height: 300px;
   background-color: red;
-}
-.max-height {
-  height: 100%;
 }
 .bg-purple {
   background: #d3dce6;
@@ -124,14 +160,12 @@ export default {
 .main-container {
   height: 100%;
   width: 100%;
-  min-height: 1000px;
   min-width: 1000px;
   position: absolute;
   background-color: #f6f6f6;
 }
 .content {
   background-color: #ffffff;
-  margin-top: 61px;
 }
 .el-menu-vertical:not(.el-menu--collapse) {
   width: 300px;
